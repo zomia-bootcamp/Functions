ad3578c8cc9080a8252eb9bfda707ba636a9b265
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-05-03 12:36:55 +0300

    Create waterDays.js

---
+++ exercizes/waterDays.js
@@ -0,0 +1,14 @@
+/*
+Let’s say we have a plant that we need to water once a week on Wednesdays. We could define a function expression to help us check the day of the week and if the plant needs to be watered:
+
+- Create a variable named plantNeedsWater using the const variable keyword.
+- Assign an anonymous function that takes in a parameter of day to plantNeedsWater.
+- In the function body add an if conditional that checks day === 'Wednesday'.
+- If the conditional is truthy, inside the if code block, use the return keyword to return true.
+- Add a condition so that on days that aren’t 'Wednesday', plantNeedsWater() should return false.
+- Call the plantNeedsWater() and pass in 'Tuesday' as an argument.
+- Test your code: Log plantNeedsWater('Tuesday') to the console. If it worked correctly, you should see false logged to the console.
+*/
+
+
+//your code goes here
