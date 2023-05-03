a5ed47c06857280e10cc3be00ed0fcea1a0bf13f
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-05-03 12:04:17 +0300

    Create defaultParameters.js

---
+++ exercizes/defaultParameters.js
@@ -0,0 +1,16 @@
+
+/*
+Assign default values to the parameters in makeShoppingList().
+
+Change the parameters of makeShoppingList() into default parameters :
+
+Assign ‘milk’ as the default value of item1.
+Assign ‘bread’ as the default value of item2.
+Assign ‘eggs’ as the default value of item3.
+*/
+
+function makeShoppingList(item1, item2, item3){
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+}
