5b5aaf57376929ed648ad7fd5b435f566ca9dcc5
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-05-03 11:52:16 +0300

    Create exercizes.js

---
+++ exercizes.js
@@ -0,0 +1,12 @@
+
+
+
+// 1. change the function so it prints any name given.
+// Add a parameter called name to the function
+// With name as a parameter, it can be used as a variable in the function body. 
+// Use string concatination to add the name variable to the message
+// A user named Jaffar needs this reminder, call the function and pass 'Jaffar' as an argument.
+
+function lunchReminder() {
+  console.log('Yoohoo, time for you lunch!');
+}
