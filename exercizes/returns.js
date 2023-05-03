73670db8cb64d8d8192b9b88077669e9389870ab
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-05-03 12:15:25 +0300

    Create returns.js

---
+++ exercizes/returns.js
@@ -0,0 +1,16 @@
+/*
+We needed to order monitors for everyone in an office and this office is conveniently arranged in a grid shape. 
+We could use a function to help us calculate the number of monitors needed!
+
+- Declare a function monitorCount() that has two parameters. The first parameter is rows and the second parameter is columns.
+
+- Let’s compute the number of monitors by multiplying rows and columns and then returning the value.
+In the function body of the function you just wrote, use the return keyword to return rows * columns.
+
+- Now that the function is defined, we can compute the number of monitors needed. Let’s say that the office has 5 rows and 4 columns.
+Declare a variable named numOfMonitors using the const keyword and assign numOfMonitors the value of invoking monitorCount() with the arguments 5 and 4.
+
+- To check that the function worked properly, log numOfMonitors to the console.
+*/
+
+//your code goes here
